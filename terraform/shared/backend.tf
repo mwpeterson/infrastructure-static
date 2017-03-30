03afe76f2ab61f4968147cd5cfb643910e4f35a7
mwpeterson <michael@mwpeterson.com>
2017-03-30 16:08:31 -0400

    sort out linked files

--- terraform/shared/backend.tf
+++ terraform/shared/backend.tf
@@ -1,11 +1,12 @@
 # 
 # initialize with `$ terraform init -backend-config 'key=project/environment'`
 # for the appropriate values of project and environment
-# e.g.: terraform init -backend-config 'key=pinkimpact-static/stage'
+# e.g.: terraform init -backend-config 'key=testproject-static/stage'
 terraform {
   backend "s3" {
-    bucket     = "gatewaychurch-static-terraform-state-dev"
-#    key        = 
+    bucket = "gatewaychurch-static-terraform-state"
+
+    #    key        = 
     encrypt    = "1"
     acl        = "private"
     region     = "us-west-2"
