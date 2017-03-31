d7ea06b060c2acacf9d1ec634a7a995ede9ef656
mwpeterson <michael@mwpeterson.com>
2017-03-30 20:01:57 -0400

    switch to shared provider file

--- terraform/prod/provider.tf
+++ terraform/prod/provider.tf
@@ -1,14 +1,1 @@
-provider "aws" {
-  alias  = "west2"
-  region = "us-west-2"
-}
-
-provider "aws" {
-  alias  = "east1"
-  region = "us-east-1"
-}
-
-provider "aws" {
-  alias  = "east2"
-  region = "us-east-2"
-}
+../shared/provider.tf
