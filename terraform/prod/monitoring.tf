1bbb61861f5f61eecd5e194e353c0ab9e0ae2da1
mwpeterson <michael@mwpeterson.com>
2017-04-24 15:47:18 -0400

    rework health check alarms so they actually are wired together

--- terraform/prod/monitoring.tf
+++ terraform/prod/monitoring.tf
@@ -1,9 +1,3 @@
-resource "aws_sns_topic" "healthcheck_alarm" {
-  provider = "aws.east1"
-
-  name = "healthcheck-alarm-${aws_s3_bucket.bucket.id}"
-}
-
 resource "aws_cloudwatch_metric_alarm" "healthcheck_bucket" {
   provider            = "aws.east1"
   alarm_name          = "healthcheck-${aws_s3_bucket.bucket.id}-alarm"
@@ -15,7 +9,11 @@ resource "aws_cloudwatch_metric_alarm" "healthcheck_bucket" {
   statistic           = "Minimum"
   threshold           = 1
 
-  alarm_actions = ["${aws_sns_topic.healthcheck_alarm.arn}"]
+  dimensions = {
+    HealthCheckId = "${aws_route53_health_check.bucket.id}"
+  }
+
+  alarm_actions = ["${data.terraform_remote_state.global.healthcheck_alarm_topic.arn}"]
 }
 
 resource "aws_cloudwatch_metric_alarm" "healthcheck_replica" {
@@ -29,18 +27,21 @@ resource "aws_cloudwatch_metric_alarm" "healthcheck_replica" {
   statistic           = "Minimum"
   threshold           = 1
 
-  alarm_actions = ["${aws_sns_topic.healthcheck_alarm.arn}"]
+  dimensions = {
+    HealthCheckId = "${aws_route53_health_check.replica.id}"
+  }
+
+  alarm_actions = ["${data.terraform_remote_state.global.healthcheck_alarm_topic.arn}"]
 }
 
 resource "aws_route53_health_check" "bucket" {
-  provider              = "aws.east1"
-  fqdn                  = "${aws_s3_bucket.bucket.bucket_domain_name}"
-  type                  = "HTTP"
-  port                  = 80
-  failure_threshold     = 3
-  request_interval      = 30
-  resource_path         = "/index.html"
-  cloudwatch_alarm_name = "${aws_cloudwatch_metric_alarm.healthcheck_bucket.alarm_name}"
+  provider          = "aws.east1"
+  fqdn              = "${aws_s3_bucket.bucket.bucket_domain_name}"
+  type              = "HTTP"
+  port              = 80
+  failure_threshold = 3
+  request_interval  = 30
+  resource_path     = "/index.html"
 
   tags = {
     environment = "${var.environment}"
@@ -49,14 +50,13 @@ resource "aws_route53_health_check" "bucket" {
 }
 
 resource "aws_route53_health_check" "replica" {
-  provider              = "aws.east1"
-  fqdn                  = "${aws_s3_bucket.replica.bucket_domain_name}"
-  type                  = "HTTP"
-  port                  = 80
-  failure_threshold     = 3
-  request_interval      = 30
-  resource_path         = "/index.html"
-  cloudwatch_alarm_name = "${aws_cloudwatch_metric_alarm.healthcheck_replica.alarm_name}"
+  provider          = "aws.east1"
+  fqdn              = "${aws_s3_bucket.replica.bucket_domain_name}"
+  type              = "HTTP"
+  port              = 80
+  failure_threshold = 3
+  request_interval  = 30
+  resource_path     = "/index.html"
 
   tags = {
     environment = "${var.environment}"
