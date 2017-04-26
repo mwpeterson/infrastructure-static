811a1c26e3607d9211dd405d37e2945a12c5a805
mwpeterson <michael@mwpeterson.com>
2017-04-25 22:21:26 -0400

    stringify the event when logging

--- lambda/s3_failover/index.js
+++ lambda/s3_failover/index.js
@@ -2,6 +2,7 @@ require('dotenv').load(); // load environment variables
 
 exports.handler = function(event, context, callback) {
     context.callbackWaitsForEmptyEventLoop = false; 
+    console.log(JSON.stringify(event,null,2));
     callback(null);
     return;
 };
