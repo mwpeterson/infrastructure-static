b5b979640f6e44985e8e9235794204c377fe8df0
mwpeterson <michael@mwpeterson.com>
2017-04-27 21:30:22 -0400

    extend healthcheck period to 15m for bucket and 3m for replica bucket avoid race condition if both buckets fail (misconfiguration, fat fingers) and lambda switches Cloudfront to replica just before replica fails. Updates to the distribution effectively invalidate the cache. These settings allow the replica to fail first so that when the bucket fails, the Lambda check of the replica will fail and Cloudfront cache will stay valid and pointing at the master bucket

--- terraform/prod/monitoring.tf
+++ terraform/prod/monitoring.tf
@@ -1,37 +1,41 @@
 resource "aws_cloudwatch_metric_alarm" "healthcheck_bucket" {
   provider            = "aws.east1"
   alarm_name          = "healthcheck-${aws_s3_bucket.bucket.id}-alarm"
+  alarm_description   = "{\"CF\":\"${aws_cloudfront_distribution.distribution.id}\"}"
   comparison_operator = "LessThanThreshold"
-  evaluation_periods  = 1
+  evaluation_periods  = 15
   metric_name         = "HealthCheckStatus"
   namespace           = "AWS/Route53"
   period              = 60
   statistic           = "Minimum"
-  threshold           = 1
+  threshold           = 15
 
   dimensions = {
     HealthCheckId = "${aws_route53_health_check.bucket.id}"
   }
 
   alarm_actions = ["${data.terraform_remote_state.global.healthcheck_alarm_topic.arn}"]
+  ok_actions    = ["${data.terraform_remote_state.global.healthcheck_alarm_topic.arn}"]
 }
 
 resource "aws_cloudwatch_metric_alarm" "healthcheck_replica" {
   provider            = "aws.east1"
   alarm_name          = "healthcheck-${aws_s3_bucket.replica.id}-alarm"
+  alarm_description   = "{\"CF\":\"${aws_cloudfront_distribution.distribution.id}\"}"
   comparison_operator = "LessThanThreshold"
-  evaluation_periods  = 1
+  evaluation_periods  = 3
   metric_name         = "HealthCheckStatus"
   namespace           = "AWS/Route53"
   period              = 60
   statistic           = "Minimum"
-  threshold           = 1
+  threshold           = 3
 
   dimensions = {
     HealthCheckId = "${aws_route53_health_check.replica.id}"
   }
 
   alarm_actions = ["${data.terraform_remote_state.global.healthcheck_alarm_topic.arn}"]
+  ok_actions    = ["${data.terraform_remote_state.global.healthcheck_alarm_topic.arn}"]
 }
 
 resource "random_id" "path" {
